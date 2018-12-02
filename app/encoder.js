d9aff70035b167d5c01c22c6d2a3e27fee1ea3ef
Ben Mangold <benmangold@gmail.com>
2018-12-02 15:08:15 -0500

    async encoding

--- app/encoder.js
+++ app/encoder.js
@@ -13,6 +13,19 @@ let outputPath;
  * @param {function} callback called upon completion
  */
 exports.encode = function(file, format, callback) {
+
+  let outputPath = gatherOutputPath(format)
+
+  writeInputFile(file, function() {
+    ffmpegCall(format, outputPath, function(val) {
+      callback(val);
+    });
+  });
+};
+
+
+
+function gatherOutputPath(format) {
   outputExtension = '';
   outputPath = 'output';
   if (format == config.MP3_CODEC) {
@@ -21,42 +34,48 @@ exports.encode = function(file, format, callback) {
   if (format == config.M4A_CODEC) {
     outputExtension = '.m4a';
   }
-  outputPath = outputPath + outputExtension;
+  return outputPath + outputExtension;
+}
 
-  writeInputFile(file, function() {
-    ffmpegCall(format, function(val) {
-      callback(val);
-    });
-  });
-};
+
 /** Writes unencoded file to disk
  * @param {string} file - Unencoded audio file
  * @param {function} callback - Function called upon completed writing
  */
 function writeInputFile(file, callback) {
   // console.log('PATH ' + __dirname);
+  // try {
+  //   fs.writeFileSync(inputPath, file, '');
+  //   callback();
+  // } catch (e) {
+  //   callback(e);
+  // }
   try {
-    fs.writeFileSync(inputPath, file, '');
-    callback();
+    fs.writeFile(inputPath, file, '', (res) => {
+      callback(res);
+    });
   } catch (e) {
     callback(e);
   }
 }
 /** Constructs and executes ffmpeg conversion cmd. Returns encoded filename
  * @param {string} format - Target audio format
+ * @param {string} outputPath - Path for output file on local disk
  * @param {string} callback - Function called upon completed conversion
  */
-function ffmpegCall(format, callback) {
+function ffmpegCall(format, outputPath, callback) {
   ffmpegConvertCommand = ffmpeg(inputPath)
     .audioCodec(format)
     .on('error', function(err) {
       // console.log('FFMPEG ERROR ' + err);
-      fs.unlinkSync(inputPath);
-      callback(config.FFMPEG_ERROR + err);
+      fs.unlink(inputPath, (err, res) => {
+        callback(config.FFMPEG_ERROR + err);
+      });
     })
     .on('end', function() {
-      fs.unlinkSync(inputPath);
-      callback(outputPath);
+      fs.unlink(inputPath, (err, res) => {
+        callback(outputPath);
+      });
     })
     .save(outputPath);
 }
